43b6c297b54937759564fa277599c7d3706ccb31
Mohammed_Almwallad <almwallad734@hotmail.com>
2020-02-22 21:43:41 +0300

    search page

---
+++ search.js
@@ -0,0 +1,52 @@
+
+let blockchain = [
+    {
+        hash: "0000c692b515949ef4d72bf7f21669eed5d0205148ce7a153faa97447a5687b4",
+        nonce: "34889",
+        height:"1",
+        timestamp:"2020-3-12 15:00",
+        prehash:"0000000000000000000000000000000000000000000000000000000000000000"
+    },
+    {
+        hash: "0000d0d27757994b430eb09043b20ea224a06de105fbe920dd7c47cc04f97c23",
+        nonce: "189257",
+        height:"2",
+        timestamp:"2020-5-13 16:40",
+        prehash:"0000c692b515949ef4d72bf7f21669eed5d0205148ce7a153faa97447a5687b4"
+    },
+    {
+        hash: "000009734f176161fa283f5a76d4a4cb96daa9688cf9bcc01a2e64ecff89e733",
+        nonce: "8928",
+        height:"3",
+        timestamp:"2020-5-16 12:50",
+        prehash:"0000d0d27757994b430eb09043b20ea224a06de105fbe920dd7c47cc04f97c23"
+    }
+];
+
+   function searchbyhash() {
+    let value = document.getElementById("searchbyhash").value;
+
+   blockchain.forEach(block => {
+       if(value === block.hash){
+        document.getElementById("hash").innerHTML = block.hash;
+        document.getElementById("nonce").innerHTML = block.nonce;
+        document.getElementById("height").innerHTML = block.height;
+        document.getElementById("timestamp").innerHTML = block.timestamp;
+        document.getElementById("prehash").innerHTML = block.prehash;
+       }
+   });
+}
+
+function searchbyheight() {
+    let value = document.getElementById("searchbyheight").value;
+
+   blockchain.forEach(block => {
+       if(value === block.height){
+        document.getElementById("hash").innerHTML = block.hash;
+        document.getElementById("nonce").innerHTML = block.nonce;
+        document.getElementById("height").innerHTML = block.height;
+        document.getElementById("timestamp").innerHTML = block.timestamp;
+        document.getElementById("prehash").innerHTML = block.prehash;
+       }
+   });
+}
