160f94eaa3bcbbbc99e4c3cb7775a782c8da9816
Mohammed_Almwallad <almwallad734@hotmail.com>
2020-03-08 01:21:09 +0300

    finished the search by hash (need one fix)

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,71 +1,68 @@
 
-let blockchain = [
-    {
-        hash: "0000c692b515949ef4d72bf7f21669eed5d0205148ce7a153faa97447a5687b4",
-        nonce: "34889",
-        height:"1",
-        timestamp:"2020-3-12 15:00",
-        prehash:"0000000000000000000000000000000000000000000000000000000000000000"
-    },
-    {
-        hash: "0000d0d27757994b430eb09043b20ea224a06de105fbe920dd7c47cc04f97c23",
-        nonce: "189257",
-        height:"2",
-        timestamp:"2020-5-13 16:40",
-        prehash:"0000c692b515949ef4d72bf7f21669eed5d0205148ce7a153faa97447a5687b4"
-    },
-    {
-        hash: "000009734f176161fa283f5a76d4a4cb96daa9688cf9bcc01a2e64ecff89e733",
-        nonce: "8928",
-        height:"3",
-        timestamp:"2020-5-16 12:50",
-        prehash:"0000d0d27757994b430eb09043b20ea224a06de105fbe920dd7c47cc04f97c23"
-    }
-];
 
-   function searchbyhash() {
+// async function GetData(){
+//     response = await fetch("http://localhost:3001/search/307c00731231e55d7ca3d12b4accb466823cbc58c20289b359525f8a69f4c89f",{
+//         method: "POST"
+//     });
+//     data = await response.json();
+
+//     console.log(data);
+    
+// }
+
+// GetData();
+
+
+ async function searchbyhash() {
+
     let value = document.getElementById("searchbyhash").value;
-    let data = "null";
-
-   blockchain.forEach(block => {
-
-    if(block.hash === value){
-        data = block;
-    }
-});
-           if(data === "null"){
+    let url = `http://localhost:3001/search/`+ value;
+    
+    let response = await fetch(url,{
+        method: "POST"
+    });
+    let block = await response.json();
+    // TODO fix the time (it gives me the current time not the time the block was made :( )
+    let time = block.timestamp;
+    time = Date(time);
+    console.log(time);
+    console.log(time.toString());
+    
+    
+           if(block === "null"){
 
             alert("there no block with this hash");
            
            }else{
-            document.getElementById("hash").innerHTML = data.hash;
-            document.getElementById("nonce").innerHTML = data.nonce;
-            document.getElementById("height").innerHTML = data.height;
-            document.getElementById("timestamp").innerHTML = data.timestamp;
-            document.getElementById("prehash").innerHTML = data.prehash;
-           }
-}
-
-function searchbyheight() {
-
-    let value = document.getElementById("searchbyheight").value;
-    let data = "null";
-    
-   blockchain.forEach(block => {
-
-    if(block.height === value){
-        data = block;
-    }
-
-});
-        if(data === "null"){
+            document.getElementById("hash").innerHTML = block.hash;
+            document.getElementById("height").innerHTML = block.height;
+            document.getElementById("timestamp").innerHTML = time.toString();
+            document.getElementById("prehash").innerHTML = block.previousHash;
             
-            alert("there no block with this height");
-           }else{
-            document.getElementById("hash").innerHTML = data.hash;
-            document.getElementById("nonce").innerHTML = data.nonce;
-            document.getElementById("height").innerHTML = data.height;
-            document.getElementById("timestamp").innerHTML = data.timestamp;
-            document.getElementById("prehash").innerHTML = data.prehash;
+            block.data.forEach(element => {
+
+                let div = document.createElement("div");
+                div.setAttribute("class", "card-body");
+                div.innerHTML = `
+                <blockquote class="blockquote mb-0">
+                <h3> ID :</h3>
+                <p id="dataid"> ${element.ID}</p>
+                <br>
+                <h3>Hash of certificate :</h3>
+                <p id="datahash"> ${element.HashOfCertificate}</p>
+                <br>
+                <h3> name:</h3>
+                <p id="dataname"> ${element.name}</p>
+                <br>
+                <h3>year :</h3>
+                <p id="datayear"> ${element.Year}</p>
+                <br>
+                <div class="dropdown-divider"></div>
+                </blockquote>`;
+
+                
+                document.getElementById("block-data").appendChild(div);
+            });
+            
            }
 }
