ac09d0118485a4bc2eee02525990d6efe533e0fd
Fahd <FahdShalhoub@gmail.com>
2020-03-08 06:24:29 +0300

    added input validation and reformatted request input

--- assets/js/addBlock.js
+++ assets/js/addBlock.js
@@ -4,21 +4,30 @@ async function sendAddBlockRequest() {
     let year = document.getElementById('graduationDate').value;
     let hash = sha256(studentId + studentName + year);
 
-    let newCertificate = [{
-        "ID": studentId,
-        "name": studentName,
-        "hash": hash,
-        "ear": year,
-    }]
-
-    //TODO: change the url to be taken from an env variable
-    let response = await fetch('http://localhost:3001/addblock',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({block: newCertificate})
-    });
-
-    alert("Successfuly added new block")
+    if(studentName == "" || studentId == "" || year == ""){
+        alert("Please fill the missing parameter(s)");
+    } else {
+        let newCertificate = [{
+            "ID": studentId,
+            "HashOfCertificate": hash,
+            "name": studentName,
+            "university": "King Saud University",
+            "Year": parseInt(year.substring(0,4)),
+        }]
+    
+        //TODO: change the url to be taken from an env variable
+        let response = await fetch('http://localhost:3001/addblock',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({block: newCertificate})
+        });
+    
+        let res = await response.json();
+        
+        alert("Successfuly added new block with hash: " + res.hash);
+    
+        window.location = './mainWindow.ejs';
+    }
 }
