52cd24c5a61d5cfbbcce1da24aa9a449761956e0
Fahd <FahdShalhoub@gmail.com>
2020-03-10 21:10:20 +0300

    Changed route form AddToBlock to AddToMempool

--- assets/js/addBlock.js
+++ assets/js/addBlock.js
@@ -1,33 +1,33 @@
-async function sendAddBlockRequest() {
+function sendAddBlockRequest() {
     let studentId = document.getElementById('studentId').value;
     let studentName = document.getElementById('studentName').value;
+    let major = document.getElementById('major').value;
     let year = document.getElementById('graduationDate').value;
-    let hash = sha256(studentId + studentName + year);
 
     if(studentName == "" || studentId == "" || year == ""){
         alert("Please fill the missing parameter(s)");
     } else {
-        let newCertificate = [{
+        let newCertificate = {
             "ID": studentId,
-            "HashOfCertificate": hash,
+            "major": major,
             "name": studentName,
             "university": "King Saud University",
-            "Year": parseInt(year.substring(0,4)),
-        }]
-    
+            "year": parseInt(year.substring(0,4)),
+        };
+        
         //TODO: change the url to be taken from an env variable
-        let response = await fetch('http://localhost:3001/addblock',{
+        fetch('http://localhost:3001/mempool/add',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({block: newCertificate})
-        });
-    
-        let res = await response.json();
-        
-        alert("Successfuly added new block with hash: " + res.hash);
-    
-        window.location = './mainWindow.ejs';
+            body: JSON.stringify(newCertificate)
+        }).then(response => {
+            document.getElementById('studentId').value = "";
+            document.getElementById('studentName').value = "";
+            document.getElementById('major').value = "";
+            document.getElementById('graduationDate').value = "";
+            alert("Successfuly added new block with hash");
+        }).catch(err => console.log(err));
     }
 }
