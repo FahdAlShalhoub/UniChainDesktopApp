6e66aaae94782210c819e8b913c213df5544c8c0
Mohammed_Almwallad <almwallad734@hotmail.com>
2020-03-11 12:46:18 +0300

    fixed some in search

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,11 +2,9 @@
  async function searchbyhash() {
 
     let value = document.getElementById("searchbyhash").value;
-    let url = `http://localhost:3001/search/`+ value;
+    let url = `http://localhost:3001/blockchain/search/`+ value;
     
-    let response = await fetch(url,{
-        method: "POST"
-    });
+    let response = await fetch(url);
     let block = await response.json();
     console.log(block.timestamp);
     let time = new Date(block.timestamp);
@@ -25,7 +23,7 @@
            }else{
             document.getElementById("hash").innerHTML = block.hash;
             document.getElementById("height").innerHTML = block.height;
-            document.getElementById("timestamp").innerHTML = block.timestamp;
+            // document.getElementById("timestamp").innerHTML = block.timestamp;
             document.getElementById("prehash").innerHTML = block.previousHash;
             
             block.data.forEach(element => {
@@ -38,13 +36,19 @@
                 <p id="dataid"> ${element.ID}</p>
                 <br>
                 <h3>Hash of certificate :</h3>
-                <p id="datahash"> ${element.HashOfCertificate}</p>
+                <p id="datahash"> ${element.hashOfCertificate}</p>
                 <br>
                 <h3> name:</h3>
                 <p id="dataname"> ${element.name}</p>
                 <br>
+                <h3>major :</h3>
+                <p id="datayear"> ${element.major}</p>
+                <br>
+                <h3>university :</h3>
+                <p id="datayear"> ${element.university}</p>
+                <br>
                 <h3>year :</h3>
-                <p id="datayear"> ${element.Year}</p>
+                <p id="datayear"> ${element.year}</p>
                 <br>
                 <div class="dropdown-divider"></div>
                 </blockquote>`;
